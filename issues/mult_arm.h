67c8bf179104eb0ee440be325de2dfb517382037
Thierry Lelegard <thierry@lelegard.fr>
2023-09-12 18:52:29 +0200

    Fixed clang symbol issues

--- issues/mult_arm.h
+++ issues/mult_arm.h
@@ -9,11 +9,11 @@
 
 #include <inttypes.h>
 
-void mul_empty(uint64_t count);
-void mul_nop_8(uint64_t count, uint64_t op1, uint64_t op2);
-void mul_nop_16(uint64_t count, uint64_t op1, uint64_t op2);
-void mul_mul_8(uint64_t count, uint64_t op1, uint64_t op2);
-void mul_mul_umulh_4(uint64_t count, uint64_t op1, uint64_t op2);
-void mul_maua_2(uint64_t count, uint64_t op1, uint64_t op2);
+extern void mul_empty(uint64_t count) asm("mul_empty");
+extern void mul_nop_8(uint64_t count, uint64_t op1, uint64_t op2) asm("mul_nop_8");
+extern void mul_nop_16(uint64_t count, uint64_t op1, uint64_t op2) asm("mul_nop_16");
+extern void mul_mul_8(uint64_t count, uint64_t op1, uint64_t op2) asm("mul_mul_8");
+extern void mul_mul_umulh_4(uint64_t count, uint64_t op1, uint64_t op2) asm("mul_mul_umulh_4");
+extern void mul_maua_2(uint64_t count, uint64_t op1, uint64_t op2) asm("mul_maua_2");
 
 #endif
