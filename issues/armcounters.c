b4804efe9b2e9997cf8100787a0590a12b6257c8
Thierry Lelegard <thierry@lelegard.fr>
2023-09-18 19:07:04 +0200

    Try CNTPCT_EL0

--- issues/armcounters.c
+++ issues/armcounters.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <locale.h>
+#include "cpu_time.h"
 #include "cpu_affinity.h"
 
 int main(int argc, char* argv[])
@@ -17,9 +18,27 @@ int main(int argc, char* argv[])
     // Make sure printf knows how to format integers.
     setlocale(LC_ALL, "en_US.UTF-8");
 
+    // Get counter frequency (not the CPU frequency).
     uint64_t cntfrq = 0;
     asm("mrs %0, cntfrq_el0" : "=r" (cntfrq));
     printf("CNTFRQ_EL0: %'" PRId64 "\n", cntfrq);
+
+    int64_t time1 = cpu_time();
+    int64_t counter1 = 0;
+    asm("mrs %0, cntpct_el0" : "=r" (counter1));
+
+    uint64_t dummy = 0xDEADBEEF;
+    for (int64_t i = 2000000000; i > 0; i--) {
+        dummy = (3 * dummy) ^ dummy;
+    }
+    printf("dummy: 0x%016" PRIu64 "\n", dummy);
+
+    int64_t time2 = cpu_time();
+    int64_t counter2 = 0;
+    asm("mrs %0, cntpct_el0" : "=r" (counter2));
+
+    printf("cpu_time: %'" PRId64 "\n", time2 - time1);
+    printf("counters: %'" PRId64 "\n", counter2 - counter1);
 
     return EXIT_SUCCESS;
 }
