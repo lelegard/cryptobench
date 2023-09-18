ab8aac42c6e00ed0b52b3ca9bde13428619f99c1
Thierry Lelegard <thierry@lelegard.fr>
2023-09-18 19:17:18 +0200

    Testing CNTVCT_EL0

--- issues/armcounters.c
+++ issues/armcounters.c
@@ -25,8 +25,8 @@ int main(int argc, char* argv[])
 
     int64_t time1 = cpu_time();
     int64_t counter1 = 0;
-    asm("mrs %0, cntpct_el0" : "=r" (counter1));
-    printf("CNTPCT_EL0: %'" PRId64 "\n", counter1);
+    asm("mrs %0, cntvct_el0" : "=r" (counter1));
+    printf("CNTVCT_EL0: %'" PRId64 "\n", counter1);
 
     uint64_t dummy = 0xDEADBEEF;
     for (int64_t i = 2000000000; i > 0; i--) {
@@ -36,11 +36,11 @@ int main(int argc, char* argv[])
 
     int64_t time2 = cpu_time();
     int64_t counter2 = 0;
-    asm("mrs %0, cntpct_el0" : "=r" (counter2));
+    asm("mrs %0, cntvct_el0" : "=r" (counter2));
 
     printf("cpu_time: %'" PRId64 "\n", time2 - time1);
     printf("counters: %'" PRId64 "\n", counter2 - counter1);
-    printf("CNTPCT_EL0: %'" PRId64 "\n", counter2);
+    printf("CNTVCT_EL0: %'" PRId64 "\n", counter2);
 
     return EXIT_SUCCESS;
 }
